61d9b63000a74cebeb8f094597ecd812acb9f39d
Dheeraj Pali <dheerajpali761@gmail.com>
2025-04-17 15:07:26 +0530

    added color shortcut in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
-export default {
+
+module.exports = {
   darkMode: 'class',
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
+      colors: {
+        'primary-blue': '#0093ef',
+      },
     },
   },
   plugins: [],
-}
+};
+
